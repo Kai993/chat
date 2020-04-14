f6327514ae7457daed05af58422faf34890859bc
Kai993 <huf0618@icloud.com>
2020-04-14 15:45:09 +0900

    1.3 チャットクライアントのHTMLとJavaScript

--- chat.go
+++ chat.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,12 +20,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 
-	if err := t.templ.Execute(w, nil); err != nil {
+	if err := t.templ.Execute(w, r); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
 	r := newRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -33,7 +37,8 @@ func main() {
 	// チャットルーム開始
 	go r.run()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", nil)
 	}
 }
